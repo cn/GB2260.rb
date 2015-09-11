eac3b1856bf8a5728b6de9a96a4fc27950baa04c
WolfLee <liyuan0228@gmail.com>
2015-09-11 17:35:14 +0800

    Add #provinces method to GB2260

--- lib/GB2260.rb
+++ lib/GB2260.rb
@@ -3,7 +3,7 @@ require "GB2260/data"
 require "GB2260/division"
 
 class GB2260
-  LATEST_YEAR = "2014".freeze
+  LATEST_YEAR = '2014'.freeze
 
   def initialize(year=nil)
     @year = year || LATEST_YEAR
@@ -11,5 +11,11 @@ class GB2260
 
   def get(code)
     Division.get(code, @year)
+  end
+
+  def provinces
+    Data.data[@year].keys
+      .select { |c| c.end_with? '0000'.freeze }
+      .map { |c| Division.get(c, @year) }
   end
 end
--- spec/GB2260_spec.rb
+++ spec/GB2260_spec.rb
@@ -8,4 +8,10 @@ describe GB2260 do
   it '#get returns a division' do
     expect(GB2260.new.get(110000)).to eq(GB2260::Division.new(110000, '北京市'))
   end
+
+  it '#provinces' do
+    expect(GB2260.new.provinces).to include(GB2260::Division.get(110000))
+    expect(GB2260.new.provinces).to_not include(GB2260::Division.get(110101))
+    expect(GB2260.new.provinces.all?(&:is_province?)).to be_truthy
+  end
 end
